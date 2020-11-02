f06683f49f3bc675e229273b75a871c8f29b3a95
im <poson_hemp@yahoo.co.jp>
2020-11-02 19:25:20 +0900

    wrap load breast cancer steps in def function

--- app/app_ml_basic/sklearn_example.py
+++ app/app_ml_basic/sklearn_example.py
@@ -6,20 +6,24 @@ def init_test():
     return "sklearn_exam_test"
 
 
-brest_cancer = load_breast_cancer()
-print(type(brest_cancer))
+def fn_load_breast_cancer():
+    brest_cancer = load_breast_cancer()
+    print(type(brest_cancer))
 
-# 説明変数
-x = brest_cancer['data']
+    # 説明変数
+    x = brest_cancer['data']
 
-# 目的変数
-y = brest_cancer['target']
+    # 目的変数
+    y = brest_cancer['target']
 
-print(type(x), x.shape)
-print(type(y), y.shape)
+    print(type(x), x.shape)
+    print(type(y), y.shape)
 
-print(x[:5])
-print(y[:5])
+    print(x[:5])
+    print(y[:5])
 
-print("benign(y=0) :", len(y[y == 0]))
-print("malignant (y=1):", len(y[y == 1]))
+    print("benign(y=0) :", len(y[y == 0]))
+    print("malignant (y=1):", len(y[y == 1]))
+
+
+fn_load_breast_cancer()
--- app/tests/test_sklearn_example.py
+++ app/tests/test_sklearn_example.py
@@ -3,3 +3,7 @@ import app_ml_basic.sklearn_example as sk_exam
 
 def test_init_test():
     assert sk_exam.init_test() == "sklearn_exam_test"
+
+
+def test_fn_load_breast_cancer():
+    sk_exam.fn_load_breast_cancer()
